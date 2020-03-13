950e38a2427b9a559723e678a68ebca7ea07e54e
moskalenkogm@gmail.com <moskalenkogm@gmail.com>
2020-03-13 08:44:39 +0300

    +

--- src/components/Log/index.jsx
+++ src/components/Log/index.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 
 const Log = ({data}) => {
   return (
-    <div>
+    <div className="log log-container">
+
+    <ul className="log__list">
       {data.map(iodata => (
-        <>
+        <li key={iodata[1]}>
          <p>{iodata[0]}</p>
          <p>{iodata[1]}</p>
-        </>
+        </li>
       ))}
-
-    </div>
+    </ul>
+</div>
   )
 }
 
